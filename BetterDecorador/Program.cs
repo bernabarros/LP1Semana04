7dd2cd4da8dcee2dc023bda1a20180deaa5105ae
bernabarros <bernardobarros.work@gmail.com>
2025-03-12 11:00:10 +0000

    Added BetterDecorador code

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -6,7 +6,19 @@ namespace BetterDecorador
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine(Decor(args[0],char.Parse(args[1]), 
+            int.Parse(args[2])));
+        }
+        private static string Decor(string s, char dec, int input_num)
+        {
+            int num = 0;
+            s = " " + s + " ";
+            while(num != input_num)
+            {
+                s = dec + s + dec;
+                ++num;
+            }
+            return s;
         }
     }
 }
