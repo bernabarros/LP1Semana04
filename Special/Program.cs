84f25acc9543c7bf8f6011348c24b5e80cc59d2b
bernabarros <bernardobarros.work@gmail.com>
2025-03-13 20:26:25 +0000

    Corrected Special project code

--- Special/Program.cs
+++ Special/Program.cs
@@ -8,19 +8,24 @@ namespace Special
         private static void Main(string[] args)
         {
             Console.Write("Insert n-th number of the special sequence:");
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine($"{GetSpecial(n)}");
+            int input_num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{GetSpecial(input_num)}");
         }
         private static int GetSpecial(int n)
         {
-            int [] spec_seq = new int [11] {0,1,1,3,5,11,21,43,85,171,341};
-            int spec_num = 0;
-            if(n != 1)
+            int spec_num;
+            if (n == 0)
             {
-                spec_num = GetSpecial(n-1) + (2 * spec_seq[n-2]);
+               spec_num = 0; 
+            }
+            else if (n == 1)
+            {
+                spec_num = 1;
             }
             else
-            spec_num += 1;
+            {
+                spec_num = GetSpecial(n-1) + (2 * GetSpecial(n-2));
+            }
             return spec_num;
         }
     }
